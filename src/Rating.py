42db8f9d62fb4a32495b474dc4ce13c622bdc74e
drestanto <dyas@live.com>
2017-11-07 01:34:48 +0700

    initiate python src

---
+++ src/Rating.py
@@ -0,0 +1,12 @@
+class Rating(object):
+    def __init__(self, kata_negatif=None, kata_positif=None):
+        self.kata_negatif = []
+        self.kata_positif = []
+
+    def get_kata_negatif(filename):
+        pass
+
+    def get_kata_positif(filename):
+        pass
+
+r = Rating()
